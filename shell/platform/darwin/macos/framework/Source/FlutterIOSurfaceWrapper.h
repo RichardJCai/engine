3d06359e3a7cbe90931ed9a353e8086218c6ec61
richardjcai <caioftherichard@gmail.com>
2020-11-20 19:19:11 -0500

    Create a wrapper for IOSurface to handle creation and binding IOSurfaces to textures / framebuffers.

---
+++ shell/platform/darwin/macos/framework/Source/FlutterIOSurfaceWrapper.h
@@ -0,0 +1,31 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Cocoa/Cocoa.h>
+
+/**
+ * FlutterIOSurfaceWrapper maintains an IOSurface
+ * and provides an interface to bind the IOSurface to a texture.
+ */
+@interface FlutterIOSurfaceWrapper : NSObject
+
+/**
+ * Bind the IOSurface to the provided texture and fbo.
+ */
+- (void)bindSurfaceToTexture:(GLuint)texture
+                             fbo:(GLuint)fbo
+                             size:(CGSize)size;
+
+/**
+ * Releases the current IOSurface if one exists 
+ * and creates a new IOSurface with the specified size.
+ */
+- (void)createOrReplaceIOSurface:(CGSize)size;
+
+/**
+ * Returns a reference to the underlying IOSurface.
+ */
+- (_Nonnull const IOSurfaceRef&)getIOSurface;
+
+@end
